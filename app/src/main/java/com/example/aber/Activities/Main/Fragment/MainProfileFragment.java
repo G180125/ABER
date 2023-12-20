12ebd3b2bf00b01956e108de21cf6552403f9395
GrayNguyen <109534682+GrayNguyen@users.noreply.github.com>
2023-12-20 15:42:47 +0700

    main profile fragment update

--- app/src/main/java/com/example/aber/Activities/Main/Fragment/MainProfileFragment.java
+++ app/src/main/java/com/example/aber/Activities/Main/Fragment/MainProfileFragment.java
@@ -1,20 +1,89 @@
 package com.example.aber.Activities.Main.Fragment;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.canhub.cropper.CropImageContract;
+import com.canhub.cropper.CropImageContractOptions;
+import com.canhub.cropper.CropImageOptions;
+import com.example.aber.FirebaseManager;
+import com.example.aber.Models.User;
 import com.example.aber.R;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class MainProfileFragment extends Fragment {
+    private static final String BUNDLE_USERID = "userID";
+    private static final String STORAGE_PATH = "avatar/";
+    private String userID;
+    private User currentUser;
+    private FirebaseManager fbManager;
+    private ProgressDialog progressDialog;
+    private CircleImageView avatar;
+    private Button uploadButton;
+    private Bitmap cropped;
+    private final ActivityResultLauncher<Intent> getImage = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+        if (result.getResultCode() == Activity.RESULT_OK) {
+            Intent data = result.getData();
+            if (data != null && data.getData() != null) {
+                Uri imageUri = data.getData();
+                launchImageCropper(imageUri);
+            }
+        }
+    });
+
+    private final ActivityResultLauncher<CropImageContractOptions> cropImage = registerForActivityResult(new CropImageContract(), result -> {
+        if (result.isSuccessful()) {
+            cropped = BitmapFactory.decodeFile(result.getUriFilePath(requireContext(), true));
+            //update avatar
+        }
+    });
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_profile, container, false);
+        View root =  inflater.inflate(R.layout.fragment_main_profile, container, false);
+        avatar = root.findViewById(R.id.avatar);
+        uploadButton = root.findViewById(R.id.upload_button);
+
+        uploadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        return root;
+    }
+
+    private void launchImageCropper(Uri uri) {
+        CropImageOptions cropImageOptions = new CropImageOptions();
+        cropImageOptions.imageSourceIncludeGallery = false;
+        cropImageOptions.imageSourceIncludeCamera = true;
+        CropImageContractOptions cropImageContractOptions = new CropImageContractOptions(uri, cropImageOptions);
+        cropImage.launch(cropImageContractOptions);
+    }
+
+    private void selectImage() {
+        getImageFile();
+    }
+
+    private void getImageFile() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        getImage.launch(intent);
     }
 }
