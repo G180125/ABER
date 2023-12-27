e7682fa5789e10e1a8ad5eabcac87c3833bced00
Admin <hungminhnguyen2202@gmail.com>
2023-12-27 10:14:03 +0700

    ui screen + loading

--- app/src/main/java/com/example/aber/Activities/SplashActivity.java
+++ app/src/main/java/com/example/aber/Activities/SplashActivity.java
@@ -1,26 +1,61 @@
 package com.example.aber.Activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
+import android.widget.ProgressBar;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.aber.Activities.Main.MainActivity;
 import com.example.aber.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SplashActivity extends AppCompatActivity {
+    private ProgressBar progressBar;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        new Handler().postDelayed(new Runnable() {
+        progressBar = findViewById(R.id.splashScreen_ProgressBar);
+        mAuth = FirebaseAuth.getInstance();
+
+        final int durationMillis = 3500;
+
+
+        ValueAnimator animator = ValueAnimator.ofInt(0, 100);
+        animator.setDuration(durationMillis);
+
+
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                int animatedValue = (int) animation.getAnimatedValue();
+                progressBar.setProgress(animatedValue);
+            }
+        });
+
+
+        animator.start();
+
+
+        progressBar.postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashActivity.this, LandingPageActivity.class));
-            }
-        } ,3500);
+                if (mAuth.getCurrentUser() != null){
+                    startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                    finish();
 
+                } else {
+                    startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                    finish();
+                }
+
+
+            }
+        }, durationMillis);
     }
 }
